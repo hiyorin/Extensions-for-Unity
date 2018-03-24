d331bfac7e617c113de0ab2d660fb72710bcd91d
hiyorin <ponkotsu.hiyorin@gmail.com>
2018-03-25 03:10:00 +0900

    add menu open directory

---
+++ Assets/Extensions/Editor/OpenDirectory.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+
+namespace Extensions.Editor
+{
+    public static class OpenDirectory
+    {
+        [MenuItem("OpenDir/DataPath")]
+        private static void OpenDataPath()
+        {
+            Process.Start(Application.dataPath);
+        }
+
+        [MenuItem("OpenDir/TemporaryCachePath")]
+        private static void OpenTemporaryCachePath()
+        {
+            Process.Start(Application.temporaryCachePath);
+        }
+
+        [MenuItem("OpenDir/PersistentDataPath")]
+        private static void OpenPersistentDataPath()
+        {
+            Process.Start(Application.persistentDataPath);
+        }
+
+        [MenuItem("OpenDir/StreamingAssetsPath")]
+        private static void OpenStreamingAssetsPath()
+        {
+            Process.Start(Application.streamingAssetsPath);
+        }
+
+        [MenuItem("OpenDir/ApplicationPath")]
+        private static void OpenApplicationPath()
+        {
+            Process.Start(EditorApplication.applicationPath);
+        }
+
+        [MenuItem("OpenDir/ApplicationContentsPath")]
+        private static void OpenApplicationContentsPath()
+        {
+            Process.Start(EditorApplication.applicationContentsPath);
+        }
+    }
+}
