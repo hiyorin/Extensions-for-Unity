7e5909abf31ebe2338b1c9196becad25d5bb9b34
hiyorin <ponkotsu.hiyorin@gmail.com>
2018-07-28 02:48:02 +0900

    [add] export script

---
+++ Assets/Editor/ExportPackage.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ExportPackage
+{
+    private readonly static string[] Paths = {
+        "Assets/Plugins/UnityExtensions",
+    };
+
+    [MenuItem("Assets/Export UnityExtensions")]
+    private static void Export()
+    {
+        AssetDatabase.ExportPackage(Paths, "ExtensionMethod-for-Unity.unitypackage", ExportPackageOptions.Recurse);
+        Debug.Log("Export complete!");
+    }
+}
