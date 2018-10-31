d49c0072dc0a50f0e870297d0eba0cf98cfbc37d
hiyorin <ponkotsu.hiyorin@gmail.com>
2018-10-31 22:35:17 +0900

    [add] float, stringbuilder extensions

---
+++ Assets/Plugins/UnityExtensions/StringBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace UnityExtensions
+{
+	public static class StringBuilderExtensions
+	{
+		public static void AppendLineFormat(this StringBuilder self, string format, params object[] args)
+		{
+			self.AppendLine(string.Format(format, args));
+		}
+	}
+}
