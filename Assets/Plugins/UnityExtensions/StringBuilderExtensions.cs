4239387f86be39fa600b17cdfd9d28cad12edb97
hiyorin <ponkotsu.hiyorin@gmail.com>
2018-12-06 02:00:53 +0900

    [add] force FL methods

--- Assets/Plugins/UnityExtensions/StringBuilderExtensions.cs
+++ Assets/Plugins/UnityExtensions/StringBuilderExtensions.cs
@@ -2,11 +2,16 @@
 
 namespace UnityExtensions
 {
-	public static class StringBuilderExtensions
-	{
-		public static void AppendLineFormat(this StringBuilder self, string format, params object[] args)
-		{
-			self.AppendLine(string.Format(format, args));
-		}
-	}
+    public static class StringBuilderExtensions
+    {
+        public static StringBuilder AppendLineFormat(this StringBuilder self, string format, params object[] args)
+        {
+            return self.AppendLine(string.Format(format, args));
+        }
+
+        public static StringBuilder AppendLineLF(this StringBuilder self, object value)
+        {
+            return self.Append(value).Append("\n");
+        }
+    }
 }
